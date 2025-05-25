d2c4475990a2f21280963d7fe29a16ac773e14f5
Snow <snow-official@outlook.com>
2025-05-25 10:15:36 +0800

    feat(private): Mybatis Plus 相关配置

---
+++ business-common/src/main/java/com/ruoyi/business/common/mybatis/component/BaseMetaObjectHandler.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) <2024> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ruoyi.business.common.mybatis.component;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.core.domain.model.LoginUser;
+import com.ruoyi.common.utils.SecurityUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Snow
+ * @date 2023/6/13 21:10
+ */
+@Slf4j
+@Component
+public class BaseMetaObjectHandler implements MetaObjectHandler {
+
+	@Override
+	public void insertFill(MetaObject metaObject) {
+		log.debug("start insert fill ....");
+
+		Long userId = SecurityUtils.getLoginUser().getUserId();
+		this.strictInsertFill(metaObject, "creator", Long.class, userId);
+		this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+	}
+
+	@Override
+	public void updateFill(MetaObject metaObject) {
+		log.debug("start update fill ....");
+
+		Long userId = SecurityUtils.getLoginUser().getUserId();
+		this.strictInsertFill(metaObject, "updater", Long.class, userId);
+		this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+	}
+
+}
---
+++ business-common/src/main/java/com/ruoyi/business/common/mybatis/domain/BaseDO.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) <2024> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ruoyi.business.common.mybatis.domain;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Snow
+ * @date 2023/6/12 17:38
+ */
+@Data
+public class BaseDO {
+
+	@TableId
+	private Long id;
+
+	private boolean deleted;
+
+	@TableField(fill = FieldFill.INSERT)
+	private Long creator;
+
+	@TableField(fill = FieldFill.INSERT)
+	private LocalDateTime createTime;
+
+	@TableField(fill = FieldFill.UPDATE)
+	private Long updater;
+
+	@TableField(fill = FieldFill.UPDATE)
+	private LocalDateTime updateTime;
+
+}
