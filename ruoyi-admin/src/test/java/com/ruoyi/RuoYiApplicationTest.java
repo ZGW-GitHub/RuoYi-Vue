41a2c398bce770a5d5ab9c7fa28918cf18c908b5
Snow <snow-official@outlook.com>
2025-05-25 10:36:44 +0800

    feat(private): 添加 Spring Boot Test

---
+++ ruoyi-admin/src/test/java/com/ruoyi/RuoYiApplicationTest.java
@@ -0,0 +1,18 @@
+package com.ruoyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zgw
+ * @date 2025/5/25
+ */
+@SpringBootTest
+public class RuoYiApplicationTest {
+
+    @Test
+    void contextTest() {
+
+    }
+
+}
