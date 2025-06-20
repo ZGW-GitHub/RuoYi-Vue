fa8f28132d75f2e2d0d3ec473782be5d7657617d
Snow <snow-official@outlook.com>
2025-06-20 09:43:06 +0800

    feat(private): common - 日志打印请求参数、返回值

---
+++ business-common/src/main/java/com/ruoyi/business/common/web/logging/RequestLoggingAspect.java
@@ -0,0 +1,37 @@
+package com.ruoyi.business.common.web.logging;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zgw
+ */
+@Slf4j
+@Aspect
+@Component
+public class RequestLoggingAspect {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Pointcut("@annotation(com.ruoyi.business.common.web.logging.RequestLogging) || @within(com.ruoyi.business.common.web.logging.RequestLogging)")
+    public void requestLoggingPointcut() {}
+
+    @Around("requestLoggingPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        log.debug("【 RequestLoggingAspect 】参数: {}", objectMapper.writeValueAsString(args));
+
+        Object result = joinPoint.proceed();
+        log.debug("【 RequestLoggingAspect 】返回值: {}", result);
+
+        return result;
+    }
+
+}
