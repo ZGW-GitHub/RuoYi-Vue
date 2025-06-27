5a5521c7caa64da62a227226ef3fa3c2e4ee164b
Snow <snow-official@outlook.com>
2025-06-27 11:15:42 +0800

    feat(private): 测试

--- ruoyi-admin/src/test/java/com/ruoyi/RuoYiApplicationTest.java
+++ ruoyi-admin/src/test/java/com/ruoyi/RuoYiApplicationTest.java
@@ -1,7 +1,12 @@
 package com.ruoyi;
 
+import com.ruoyi.common.core.domain.model.LoginUser;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * @author zgw
@@ -12,7 +17,21 @@ public class RuoYiApplicationTest {
 
     @Test
     void contextTest() {
+    }
 
+    @BeforeAll
+    static void beforeAll() {
+        LoginUser loginUser = new LoginUser();
+        loginUser.setUserId(105L);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+        // authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        SecurityContextHolder.clearContext();
     }
 
 }
---
+++ ruoyi-admin/src/test/java/com/ruoyi/business/BusinessTest.java
@@ -0,0 +1,9 @@
+package com.ruoyi.business;
+
+import com.ruoyi.RuoYiApplicationTest;
+
+/**
+ * @author zgw
+ */
+public class BusinessTest extends RuoYiApplicationTest {
+}
