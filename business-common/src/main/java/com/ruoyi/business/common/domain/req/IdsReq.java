46001af997febcd72937a703b073b86e3f9589af
Snow <snow-official@outlook.com>
2025-07-08 13:35:35 +0800

    feat(private): common

---
+++ business-common/src/main/java/com/ruoyi/business/common/domain/req/IdsReq.java
@@ -0,0 +1,15 @@
+package com.ruoyi.business.common.domain.req;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Snow
+ */
+@Data
+public class IdsReq {
+
+    private List<Long> ids;
+
+}
