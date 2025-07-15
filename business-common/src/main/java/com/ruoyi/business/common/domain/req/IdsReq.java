b8e93f827601d6259900096593824806abdb64b4
Snow <snow-official@outlook.com>
2025-07-15 21:26:52 +0800

    feat(private): web 增强

--- business-common/src/main/java/com/ruoyi/business/common/domain/req/IdsReq.java
+++ business-common/src/main/java/com/ruoyi/business/common/domain/req/IdsReq.java
@@ -1,8 +1,10 @@
 package com.ruoyi.business.common.domain.req;
 
+import cn.hutool.core.collection.CollUtil;
 import lombok.Data;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Snow
@@ -10,6 +12,10 @@ import java.util.List;
 @Data
 public class IdsReq {
 
-    private List<Long> ids;
+    private List<String> ids;
+
+    public List<Long> ids() {
+        return CollUtil.emptyIfNull(ids).stream().map(Long::valueOf).collect(Collectors.toList());
+    }
 
 }
