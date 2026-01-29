dc2b9ab6cc0f5b186938c1b66a72c1d67845e77c
Snow <snow-official@outlook.com>
2026-01-29 14:48:25 +0800

    feat(private): template 模块示例代码

---
+++ business-template/src/main/java/com/ruoyi/business/template/dal/mapper/TemplateMapper.java
@@ -0,0 +1,12 @@
+package com.ruoyi.business.template.dal.mapper;
+
+import com.ruoyi.business.common.mybatis.mapper.BaseMapper;
+import com.ruoyi.business.template.dal.dos.Template;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Snow
+ */
+@Mapper
+public interface TemplateMapper extends BaseMapper<Template> {
+}
---
+++ business-template/src/main/java/com/ruoyi/business/template/service/TemplateService.java
@@ -0,0 +1,10 @@
+package com.ruoyi.business.template.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ruoyi.business.template.dal.dos.Template;
+
+/**
+ * @author Snow
+ */
+public interface TemplateService extends IService<Template> {
+}
