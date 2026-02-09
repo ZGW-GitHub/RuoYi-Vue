81d1ffc8a507121a889e1c69080c6a56c74afb70
Snow <snow-official@outlook.com>
2026-02-09 16:43:32 +0800

    feat(private): 部门

---
+++ business-common/src/main/java/com/ruoyi/business/common/domain/dto/DeptDTO.java
@@ -0,0 +1,17 @@
+package com.ruoyi.business.common.domain.dto;
+
+import lombok.Data;
+
+/**
+ * @author Snow
+ */
+@Data
+public class DeptDTO {
+
+    private String deptId;
+
+    private String deptName;
+
+    private String label;
+
+}
