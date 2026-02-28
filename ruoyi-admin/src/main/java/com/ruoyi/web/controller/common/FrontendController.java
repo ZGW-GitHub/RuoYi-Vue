0d7526fa1bd4cc1c8d7cef2df969d004d2976e92
Snow <snow-official@outlook.com>
2026-02-28 09:37:04 +0800

    feat(整合前端): 前端路由跳转到 index

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/FrontendController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/FrontendController.java
@@ -12,7 +12,11 @@ public class FrontendController {
     /**
      * 匹配所有前端路由（ 避免 404 ）
      */
-    @GetMapping(value = {"/", "/login", "/home", "/menu/**"})
+    @SuppressWarnings("all")
+    @GetMapping(value = {
+            "/", "/login", "/home", "/menu/**",
+            "/{path:[^.]*}", "/**/{path:[^.]*}" // 不包含点号（作用：只拦截前端路由）
+    })
     public String forward() {
         return "forward:/index.html";
     }
